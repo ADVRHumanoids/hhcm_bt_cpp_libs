3128cbd3c6f859c73c6331f4e882f2d10795ca94
Davide Torielli <toridebraus@gmail.com>
2025-01-10 19:03:03 +0100

    options to check condition for some seconds

--- include/hhcm_bt_cpp_libs/CheckGripperGrasp.h
+++ include/hhcm_bt_cpp_libs/CheckGripperGrasp.h
@@ -1,13 +1,15 @@
 #ifndef HHCM_BT_CPP_LIBS_CHECK_GRIPPER_GRASP_H
 #define HHCM_BT_CPP_LIBS_CHECK_GRIPPER_GRASP_H
 
+#include <chrono>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-#include <behaviortree_cpp/condition_node.h>
+#include <behaviortree_cpp/action_node.h>
 
 namespace hhcm_bt {
 
-class CheckGripperGrasp : public BT::ConditionNode {
+class CheckGripperGrasp : public BT::StatefulActionNode
+{
 
 public:
     CheckGripperGrasp(const std::string& name, const BT::NodeConfiguration& config);
@@ -15,7 +17,16 @@ public:
 
     static BT::PortsList providedPorts();
 
-    BT::NodeStatus tick() override;
+    BT::NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
+
+private:
+    double requested_effort;
+    double requested_effort_max_err;
+    int msec;
+    double joint_vel_lim;
+    std::chrono::system_clock::time_point deadline;
 };
 
 } //namespace
--- src/CheckGripperGrasp.cpp
+++ src/CheckGripperGrasp.cpp
@@ -3,7 +3,7 @@
 using hhcm_bt::CheckGripperGrasp;
 
 CheckGripperGrasp::CheckGripperGrasp(const std::string& name, const BT::NodeConfiguration& config)
-    : BT::ConditionNode(name, config) {
+    : BT::StatefulActionNode(name, config) {
 
 
 }
@@ -12,21 +12,39 @@ BT::PortsList CheckGripperGrasp::providedPorts() {
     return {
         BT::InputPort<sensor_msgs::JointState>("gripper_state", "{=}", ""),
         BT::InputPort<double>("requested_effort"),
-        BT::InputPort<double>("requested_effort_max_err", 1, ""),
+        BT::InputPort<double>("requested_effort_max_err"),
+        BT::InputPort<double>("joint_vel_lim", 0.1, "joint velocity under which we start checking the condition, since otherwisw the gripper is cosndiered still moving"),
+        BT::InputPort<int>("msec", 0, "how much time (in ms) to check for the condition")
     };
 }
 
-BT::NodeStatus CheckGripperGrasp::tick() {
+BT::NodeStatus CheckGripperGrasp::onStart() {
 
     auto requested_effort_exp = getInput<double>("requested_effort");
-    double requested_effort;
     if (requested_effort_exp) {
         requested_effort = requested_effort_exp.value();
     } else {
         throw std::runtime_error("CheckGripperGrasp: no requested_effort key found in the bb");
     }
 
-    auto requested_effort_max_err = getInput<double>("requested_effort_max_err");
+    auto requested_effort_max_err_exp = getInput<double>("requested_effort_max_err");
+    if (requested_effort_max_err) {
+        requested_effort_max_err = requested_effort_max_err_exp.value();
+    } else {
+        throw std::runtime_error("CheckGripperGrasp: no requested_effort_max_err key found in the bb");
+    }
+    auto msec_exp = getInput<int>("msec");
+    msec = msec_exp ? msec_exp.value() : 0;
+
+    auto joint_vel_lim_exp = getInput<double>("joint_vel_lim");
+    joint_vel_lim = joint_vel_lim_exp ? joint_vel_lim_exp.value() : 0.1;
+    
+    deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+
+    return BT::NodeStatus::RUNNING;
+}
+
+BT::NodeStatus CheckGripperGrasp::onRunning() {
     
     if(auto gripper_state_exp = getLockedPortContent("gripper_state"))
     {
@@ -34,13 +52,41 @@ BT::NodeStatus CheckGripperGrasp::tick() {
         {
             throw std::runtime_error("CheckGripperGrasp: no gripper_state key found in the bb");
         }
-        else if(const sensor_msgs::JointState* gripper_state = gripper_state_exp->castPtr<sensor_msgs::JointState>())
+
+        if(const sensor_msgs::JointState* gripper_state = gripper_state_exp->castPtr<sensor_msgs::JointState>())
         {
 
+            if (gripper_state->velocity[0] >= joint_vel_lim) {
+                std::cout << "[" << name() << "] Check Gripper grasp wait for the gripper joint to stop, velocity is" <<
+                    gripper_state->velocity[0] << std::endl; 
+                deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(msec);
+                return BT::NodeStatus::RUNNING;
+            }
+
             double err = std::abs(requested_effort - std::abs(gripper_state->effort[0])); //keep abs since dagana eff is inverted
-            if (gripper_state->position[0] < 0.95 && //1.0 is open, 0.0 is closed 
-                err < requested_effort_max_err.value()) {
-                return BT::NodeStatus::SUCCESS;
+            if (gripper_state->position[0] < 0.95 && //0 is open, 1.0 is closed 
+                err < requested_effort_max_err) 
+            {
+                if (std::chrono::system_clock::now() >= deadline ) {
+                    return BT::NodeStatus::SUCCESS;
+
+                } else {
+                    std::cout << "[" << name() << "] Check Gripper grasp running\n" << "Joint state value are: \n";
+                    for (int i=0; i<gripper_state->name.size(); i++) {
+                        std::cout << 
+                            "name: " << gripper_state->name[i] << "\n" <<
+                            "position: " << gripper_state->position[i] << "\n" <<
+                            "velocity: " << gripper_state->velocity[i] << "\n" <<
+                            "effort: " << std::abs(gripper_state->effort[i]) << "\n";
+                    }
+                    std::cout << "requested_effort: " << requested_effort << "\n";
+                    std::cout << "effort error: " << err << "\n";
+                    std::cout << "requested_effort_max_err: " << requested_effort_max_err << "\n" 
+                        << gripper_state->position[0] << std::endl;
+
+                    return BT::NodeStatus::RUNNING;
+                }
+
             } else {
                 std::cout << "[" << name() << "] Check Gripper grasp fail\n" << "Joint state value are: \n";
                 for (int i=0; i<gripper_state->name.size(); i++) {
@@ -52,13 +98,19 @@ BT::NodeStatus CheckGripperGrasp::tick() {
                 }
                 std::cout << "requested_effort: " << requested_effort << "\n";
                 std::cout << "effort error: " << err << "\n";
-                std::cout << "requested_effort_max_err: " << requested_effort_max_err.value() << "\n" 
+                std::cout << "requested_effort_max_err: " << requested_effort_max_err << "\n" 
                     << gripper_state->position[0] << std::endl;
                 }
+        } else {
+            std::cout << "[" << name() << "] Check Gripper grasp running castPtr failed" << std::endl;
         }
     } else {
-        std::cout << "getLockedPortContent failed" << std::endl;
+         std::cout << "[" << name() << "] Check Gripper grasp running getLockedPortContent failed" << std::endl;
     }
     return BT::NodeStatus::FAILURE;
 
+}
+
+void CheckGripperGrasp::onHalted() {
+    std::cout << "[" << name() << "] Check Gripper grasp halted" << std::endl;
 }
